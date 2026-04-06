c2e35a89d4db36dd7d1d15e02c35e56d194e0215
gatay22 <tangguljatiroto@gmail.com>
2026-04-06 19:53:32 +0700

    Update PreHardmodeChaos.cs

--- PreHardmodeChaos.cs
+++ PreHardmodeChaos.cs
@@ -3,99 +3,104 @@ using TerrariaApi.Server;
 using TShockAPI;
 using Microsoft.Xna.Framework;
 using Terraria.ID;
+using System;
+using System.Linq;
 
-namespace PreHardmodeChaos;
-
-[ApiVersion(2, 1)]
-public class PreHardmodeChaos : TerrariaPlugin
+namespace PreHardmodeChaos
 {
-    public override string Name => "Pre-Hardmode Boss Variations";
-    public override string Author => "AI Assistant";
-    public override string Description => "Serangan variasi untuk semua boss Pre-Hardmode";
-    public override Version Version => new(1, 0, 0);
-
-    public PreHardmodeChaos(Main game) : base(game) { }
-
-    public override void Initialize()
+    [ApiVersion(2, 1)]
+    public class PreHardmodeChaos : TerrariaPlugin
     {
-        ServerApi.Hooks.GameUpdate.Register(this, OnUpdate);
-    }
+        public override string Name => "Pre-Hardmode Boss Variations";
+        public override string Author => "AI Assistant";
+        public override string Description => "Serangan variasi untuk semua boss Pre-Hardmode";
+        public override Version Version => new(1, 0, 0);
 
-    private void OnUpdate(EventArgs args)
-    {
-        if (Main.gameMenu || !Netplay.AnyPlayers) return;
+        public PreHardmodeChaos(Main game) : base(game) { }
 
-        for (int i = 0; i < Main.maxNPCs; i++)
+        public override void Initialize()
         {
-            NPC npc = Main.npc[i];
-            if (!npc.active) continue;
+            ServerApi.Hooks.GameUpdate.Register(this, OnUpdate);
+        }
 
-            switch (npc.type)
+        private void OnUpdate(EventArgs args)
+        {
+            // Perbaikan: Cek pemain menggunakan TShock atau Main.player
+            if (Main.gameMenu || !Main.player.Any(p => p != null && p.active)) return;
+
+            for (int i = 0; i < Main.maxNPCs; i++)
             {
-                case NPCID.KingSlime:
-                    // King Slime melompat dan menciptakan ledakan slime berduri (Spiked Slime)
-                    if (Main.GameUpdateCount % 240 == 0) // Setiap 4 detik
-                        ShootProjectiles(npc, ProjectileID.SpikedSlimeSpike, 6, 10f);
-                    break;
+                NPC npc = Main.npc[i];
+                if (!npc.active) continue;
 
-                case NPCID.EyeofCthulhu:
-                    // Saat HP < 50%, menembakkan Servant of Cthulhu lebih cepat
-                    if (npc.life < (npc.lifeMax / 2) && Main.rand.NextBool(150))
-                        NPC.NewNPC(npc.GetSource_FromAI(), (int)npc.Center.X, (int)npc.Center.Y, NPCID.ServantofCthulhu);
-                    break;
+                switch (npc.type)
+                {
+                    case NPCID.KingSlime:
+                        if (Main.GameUpdateCount % 240 == 0)
+                            ShootProjectiles(npc, ProjectileID.SpikedSlimeSpike, 6, 10f);
+                        break;
 
-                case NPCID.EaterofWorldsHead:
-                    // Kepala Eater menembakkan Vile Spit saat mendekat
-                    if (Main.rand.NextBool(200))
-                        ShootProjectiles(npc, ProjectileID.VileSpitHostile, 1, 8f);
-                    break;
+                    case NPCID.EyeofCthulhu:
+                        // Perbaikan: Ganti NextBool(150) dan hapus GetSource_FromAI
+                        if (npc.life < (npc.lifeMax / 2) && Main.rand.Next(150) == 0)
+                            NPC.NewNPC(null, (int)npc.Center.X, (int)npc.Center.Y, NPCID.ServantofCthulhu);
+                        break;
 
-                case NPCID.BrainofCthulhu:
-                    // Brain menembakkan laser ilusi (seperti Creeper)
-                    if (npc.life < (npc.lifeMax / 2) && Main.rand.NextBool(100))
-                        ShootProjectiles(npc, ProjectileID.BrainLaser, 3, 7f);
-                    break;
+                    case NPCID.EaterofWorldsHead:
+                        // Perbaikan: VileSpitHostile (ID 307)
+                        if (Main.rand.Next(200) == 0)
+                            ShootProjectiles(npc, 307, 1, 8f);
+                        break;
 
-                case NPCID.QueenBee:
-                    // Queen Bee menjatuhkan sarang lebah (Bee Hive) dari langit-langit saat marah
-                    if (Main.GameUpdateCount % 300 == 0)
-                        ShootProjectiles(npc, ProjectileID.Stinger, 5, 5f);
-                    break;
+                    case NPCID.BrainofCthulhu:
+                        // Perbaikan: BrainLaser (ID 290)
+                        if (npc.life < (npc.lifeMax / 2) && Main.rand.Next(100) == 0)
+                            ShootProjectiles(npc, 290, 3, 7f);
+                        break;
 
-                case NPCID.SkeletronHead:
-                    // Skeletron mengeluarkan gelombang Shadow Flame
-                    if (Main.GameUpdateCount % 400 == 0)
-                        ShootProjectiles(npc, ProjectileID.ShadowFlameArrow, 8, 12f);
-                    break;
+                    case NPCID.QueenBee:
+                        if (Main.GameUpdateCount % 300 == 0)
+                            ShootProjectiles(npc, ProjectileID.Stinger, 5, 5f);
+                        break;
 
-                case NPCID.WallofFlesh:
-                    // Wall of Flesh menembakkan Hungry lebih sering dan Laser ungu
-                    if (Main.GameUpdateCount % 180 == 0)
-                        ShootProjectiles(npc, ProjectileID.DeathLaser, 2, 15f);
-                    break;
+                    case NPCID.SkeletronHead:
+                        if (Main.GameUpdateCount % 400 == 0)
+                            ShootProjectiles(npc, ProjectileID.ShadowFlameArrow, 8, 12f);
+                        break;
+
+                    case NPCID.WallofFlesh:
+                        if (Main.GameUpdateCount % 180 == 0)
+                            ShootProjectiles(npc, ProjectileID.DeathLaser, 2, 15f);
+                        break;
+                }
             }
         }
-    }
 
-    private void ShootProjectiles(NPC npc, int projId, int count, float speed)
-    {
-        for (int i = 0; i < count; i++)
+        private void ShootProjectiles(NPC npc, int projId, int count, float speed)
         {
-            float rotation = (float)(Main.rand.NextDouble() * Math.PI * 2);
-            Vector2 vel = new Vector2((float)Math.Cos(rotation) * speed, (float)Math.Sin(rotation) * speed);
-            
-            int p = Projectile.NewProjectile(npc.GetSource_FromAI(), npc.Center, vel, projId, 15, 0f, Main.myPlayer);
-            Main.projectile[p].hostile = true;
-            Main.projectile[p].friendly = false;
-            
-            NetMessage.SendData((int)PacketTypes.ProjectileNew, -1, -1, null, p);
+            for (int i = 0; i < count; i++)
+            {
+                float rotation = (float)(Main.rand.NextDouble() * Math.PI * 2);
+                Vector2 vel = new Vector2((float)Math.Cos(rotation) * speed, (float)Math.Sin(rotation) * speed);
+                
+                // Perbaikan: Gunakan overload NewProjectile yang didukung Server (tanpa EntitySource)
+                int p = Projectile.NewProjectile(null, npc.Center.X, npc.Center.Y, vel.X, vel.Y, projId, 15, 0f, Main.myPlayer);
+                
+                if (p < 1000) // Pastikan ID proyektil valid
+                {
+                    Main.projectile[p].hostile = true;
+                    Main.projectile[p].friendly = false;
+                    
+                    // Beritahu client bahwa ada proyektil baru
+                    NetMessage.SendData((int)PacketTypes.ProjectileNew, -1, -1, null, p);
+                }
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) ServerApi.Hooks.GameUpdate.Deregister(this, OnUpdate);
+            base.Dispose(disposing);
         }
     }
-
-    protected override void Dispose(bool disposing)
-    {
-        if (disposing) ServerApi.Hooks.GameUpdate.Deregister(this, OnUpdate);
-        base.Dispose(disposing);
-    }
 }
-
